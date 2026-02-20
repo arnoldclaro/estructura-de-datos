7d2513a6dc6cd3a0274f315e5633bfbdf58d82d4
arnold claro <arnoldclaro0613@hotmail.com>
2026-02-20 07:53:52 -0500

    base de monitoreo

---
+++ sistema-monitoreo/src/Main.java
@@ -0,0 +1,13 @@
+import  java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        temperaturasSensor[][] = new int[3][3];
+
+        for(int i = 0; i < temperaturasSensor.length; i++){
+
+        }
+    }
+}
+
+public static void
