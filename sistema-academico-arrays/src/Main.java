d5513aa77be2a1e44a7ce09e7936599c142b827c
arnold claro <arnoldclaro0613@hotmail.com>
2026-02-20 07:32:05 -0500

    se termina el programa del sistema academico

--- sistema-academico-arrays/src/Main.java
+++ sistema-academico-arrays/src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -7,17 +6,51 @@ public class Main {
         double notas[] = new double[10];
 
         for (int i = 0; i < notas.length; i++) {
-            System.out.println("ingrese la nota del estudiante");
+            System.out.print("Ingrese la nota del estudiante " + (i + 1) + ": ");
             notas[i] = input.nextDouble();
         }
-        System.out.println("EL promedio de las notas es: "+ promedio(notas));
+
+
+        System.out.println("\n--- RESULTADOS ---");
+        System.out.println("Promedio: " + promedio(notas));
+        System.out.println("Nota Máxima: " + notaMaxima(notas));
+        System.out.println("Nota Mínima: " + notaMinima(notas));
+        System.out.println("Cantidad de Aprobados: " + cantidadAprobados(notas));
     }
 
-    public static double promedio(double[] notas){
+    public static double promedio(double[] notas) {
         double suma = 0;
-        for(int i = 0; i < notas.length; i++){
-            suma += notas[i];
-        }
+        for (double nota : notas) { suma += nota; }
         return suma / notas.length;
     }
+
+    public static double notaMaxima(double[] notas) {
+        double notaMax = notas[0];
+        for (double nota : notas) {
+            if (nota > notaMax) notaMax = nota;
+        }
+        return notaMax;
+    }
+
+    public static double notaMinima(double[] notas) {
+        double notaMin = notas[0];
+        for (double nota : notas) {
+            if (nota < notaMin) notaMin = nota;
+        }
+        return notaMin;
+    }
+
+
+    public static byte cantidadAprobados(double notas[]) {
+        byte cant = 0;
+        for (double nota : notas) {
+            if (nota >= 3) {
+                cant++;
+            }
+        }
+        return cant;
+    }
 }
+
+
+
