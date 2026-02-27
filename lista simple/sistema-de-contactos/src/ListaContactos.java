9bf8178867e39e15e9dde2dc01f4f74e4928c963
arnold claro <arnoldclaro0613@hotmail.com>
2026-02-27 07:33:16 -0500

    ultimos ajustes del sistema de contactos

---
+++ lista simple/sistema-de-contactos/src/ListaContactos.java
@@ -0,0 +1,63 @@
+public class ListaContactos {
+    private Nodo cabeza;
+
+    public ListaContactos(Nodo cabeza){
+        cabeza = null;
+    }
+
+    public void insertarFinal(Contactos Contactos){
+        Nodo nuevo = new Nodo(Contactos);
+
+        if(cabeza == null){
+            cabeza = nuevo;
+        }
+        else{
+            Nodo actual = cabeza;
+            while (actual.siguiente != null){
+                actual = actual.siguiente;
+            }
+            actual.siguiente = nuevo;
+        }
+    }
+    public Contactos buscar(String nombre){
+        Nodo actual = cabeza;
+
+        while(actual!=null){
+            if (actual.getDato().getNombre().equalsIgnoreCase(nombre)){
+                return actual.getDato();
+            }
+            actual = actual.getSiguiente();
+        }
+        return null;
+    }
+    public boolean eliminar(String nombre){
+        if (cabeza == null){
+            return false;
+        }
+        //Si es el primero
+        if (cabeza.getDato().getNombre().equalsIgnoreCase(nombre)){
+            cabeza = cabeza.getSiguiente();
+            return true;
+        }
+
+        Nodo actual = cabeza;
+
+        while (actual.getSiguiente() != null){
+            if (actual.getDato().getNombre().equalsIgnoreCase(nombre)){
+                actual.setSiguiente(actual.getSiguiente().getSiguiente());
+                return true;
+            }
+            actual.setSiguiente(actual.getSiguiente());
+        }
+        return false;
+    }
+
+    public void listar(){
+        Nodo actual = cabeza;
+
+        while(actual!=null){
+            System.out.println(actual.getDato());
+            actual.setSiguiente(actual.getSiguiente());
+        }
+    }
+}
--- lista simple/sistema-de-contactos/src/Nodo.java
+++ lista simple/sistema-de-contactos/src/Nodo.java
@@ -1,7 +1,25 @@
 public class Nodo {
+    public Nodo siguiente;
     private Contactos dato;
 
     public Nodo(Contactos dato) {
         this.dato = dato;
     }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public Contactos getDato() {
+        return dato;
+    }
+
+    public void setDato(Contactos dato) {
+        this.dato = dato;
+    }
 }
+
