fc7dd029ee7dd9d43256e05d384f4ded95b2699c
arnold claro <arnoldclaro0613@hotmail.com>
2026-03-03 07:17:34 -0500

    sistema-de-conctactos-arraylist

---
+++ lista simple/sistema-de-contactos/sistema-de-contactos-arraylist/src/Contactos.java
@@ -0,0 +1,46 @@
+public class Contactos {
+    String nombre;
+    String telefono;
+    String correo;
+
+    public Contactos(String nombre, String telefono, String correo) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return "Contactos{" +
+                "nombre='" + nombre + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", correo='" + correo + '\'' +
+                '}';
+    }
+}
+
+
