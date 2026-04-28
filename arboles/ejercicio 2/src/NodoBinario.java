a0b380afa2385e88d9f9ee475c07a043b3244da9
arnoldclaro <arnoldclaro0613@hotmail.com>
2026-04-28 05:29:07 -0500

    Merge remote-tracking branch 'origin/main'

---
+++ arboles/ejercicio 2/src/NodoBinario.java
@@ -0,0 +1,11 @@
+public class NodoBinario {
+    int dato;
+    NodoBinario izquierdo;
+    NodoBinario derecho;
+
+    public NodoBinario(int dato) {
+        this.dato = dato;
+        this.izquierdo = null;
+        this.derecho = null;
+    }
+}
